7aa56ddddf45790337f1eb7d4489f57da0095e9c
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-08 08:53:02 -0300

    add axios and loading sensors on places

--- pages/place.js
+++ pages/place.js
@@ -1,14 +1,33 @@
-var ul = document.getElementById('place')
+ //api
+ const getSensors= async() => {
+   try {
+    const {data}  = await axios.get('http://localhost:3456/sensor')
+    console.log(data)
+
+    return data
+   } catch (error) {
+        console.error(error)
+   }
+ }
+   
+
+let ul = document.getElementById('place')
 
 
+const loadList = async() => {
+    const sensors = await getSensors()
 
-for (i = 0; i < 4; i++) {
-    let li = document.createElement('li')
-    
-    li.innerHTML = `<li class="place__list">
-                        <span class="place__name">name</span>
-                        <canvas class="place__graph"></canvas>
-                    </li>`
+    console.log(sensors)
+    for (let index in sensors) {
+        let li = document.createElement('li')
+        
+        li.innerHTML = `<li class="place__list">
+                            <span class="place__name">${sensors[index].description}</span>
+                            <canvas class="place__graph"></canvas>
+                        </li>`
 
-    ul.appendChild(li)
- } 
+        ul.appendChild(li)
+    } 
+}
+
+loadList()
