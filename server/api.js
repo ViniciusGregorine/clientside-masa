def55ca9458e052e5890d94165d6ec8edfeab809
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-17 14:06:17 -0300

    reaf: change axios querys to file server/api.js

---
+++ server/api.js
@@ -0,0 +1,22 @@
+export const getPlace= async() => {
+    try {
+     const {data}  = await axios.get('http://localhost:3456/place')
+     console.log(data)
+ 
+     return data
+    } catch (error) {
+         console.error(error)
+    }
+  }
+ 
+export const getReadings= async() => {
+     try {
+      const {data}  = await axios.get('http://localhost:3456/reading')
+      console.log(data)
+  
+      return data
+     } catch (error) {
+          console.error(error)
+     }
+   }
+    
