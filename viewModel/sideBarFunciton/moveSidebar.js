1bb8d4c4e7c9c8b236861142323b5767b4d3563c
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-31 16:03:19 -0300

    add: add navbar to index.html

---
+++ viewModel/sideBarFunciton/moveSidebar.js
@@ -0,0 +1,16 @@
+function moveSidebar(){
+    let sidebar = document.getElementById('sidebar');
+    let width = sidebar.style.width;
+
+    if(width === ""){
+      document.getElementById("sidebar").style.width = "190px";
+      document.getElementById("burgerMenu").style.position = "fixed";
+      document.getElementById("burgerMenu").style.color = "black";
+
+      
+    }else{
+      document.getElementById("sidebar").style.width = "";
+      document.getElementById("burgerMenu").style.position = "relative"; 
+      document.getElementById("body").style.background = "white"; 
+    }
+  }
