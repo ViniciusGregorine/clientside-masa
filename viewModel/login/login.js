9eb6a2c55a6d3b067dc1746e1667916c9a5c5b13
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-12 16:07:32 -0300

    feat: create login function on login page

---
+++ viewModel/login/login.js
@@ -0,0 +1,24 @@
+import { loginUser } from "../../model/server/api.js"
+
+const loginButton = document.getElementById('register__button')
+loginButton.addEventListener('click', loginHandler)
+
+async function loginHandler(){
+    const url = 'http://localhost:5500/view/pages/place/place.html'
+
+    const password = document.getElementById('password_input').value
+    const email = document.getElementById('email_input').value
+
+    try{
+        const status = await loginUser(email, password)
+        if(status === 200){
+            document.location.href = url;
+        }else{
+            document.getElementById('error__message').style.display = "block"
+            loginButton.style.marginTop = '10px'
+        }   
+   }
+   catch{
+        alert('Ocorreu algum erro')
+   }
+}
