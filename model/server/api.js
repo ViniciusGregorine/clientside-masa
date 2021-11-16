00a8a45fe3435ce959d406e8df04513387b73989
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-16 17:14:06 -0300

    refactor: add axios baseURL

--- model/server/api.js
+++ model/server/api.js
@@ -1,8 +1,11 @@
 import { login } from "./auth.js"
 
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = 'http://localhost:3456'
+
 export const getPlace= async() => {
     try {
-     const {data}  = await axios.get('http://localhost:3456/place')
+     const {data}  = await axios.get('/place')
  
      return data.message
     } catch (error) {
@@ -12,7 +15,7 @@ export const getPlace= async() => {
 
 export const getSensor= async() => {
      try {
-          const {data}  = await axios.get('http://localhost:3456/sensor')
+          const {data}  = await axios.get('/sensor')
 
           return data.message
      } catch (error) {
@@ -22,7 +25,7 @@ export const getSensor= async() => {
 
 export const getReadings= async() => {
      try {
-      const {data}  = await axios.get('http://localhost:3456/reading')
+      const {data}  = await axios.get('/reading')
       console.log(data)
   
       return data.message 
@@ -33,7 +36,7 @@ export const getReadings= async() => {
 
 export const getReadingsByPlaceId = async(placeId) => {
 	try {
-	 const {data} = await axios.get(`http://localhost:3456/reading${placeId}`)
+	 const {data} = await axios.get(`/reading${placeId}`)
 	 return data.message 
 	} catch (error) {
 		 console.error(error)
@@ -43,7 +46,7 @@ export const getReadingsByPlaceId = async(placeId) => {
   
 export const loginUser = async(email, password) => {
 	try {
-	 const {data}  = await axios.get(`http://localhost:3456/login`, {
+	 const {data}  = await axios.get(`/login`, {
           headers: {
                'password': `${password}`,
                'email': `${email}`
