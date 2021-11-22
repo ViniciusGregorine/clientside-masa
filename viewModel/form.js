9f7189e6b2ad933fe942914668cc665c10cf088b
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-11-22 14:16:08 -0300

    add register styles and popup

---
+++ viewModel/form.js
@@ -0,0 +1,7 @@
+function openForm(){
+    document.getElementById("register").style.display = "block"
+}
+
+function closeForm() {
+    document.getElementById("register").style.display = "none";
+  }
