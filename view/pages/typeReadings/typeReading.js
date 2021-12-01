9b58710d81589f98bf333bcad436662f353a877a
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-12-01 16:28:25 -0300

    add `submitTypeReading` funciton

--- view/pages/typeReadings/typeReading.js
+++ view/pages/typeReadings/typeReading.js
@@ -1,4 +1,4 @@
-import { getTypeReadings } from "../../../model/server/api.js"
+import { getTypeReadings, postTypeReading } from "../../../model/server/api.js"
 
 async function loadTypeReadings(){
     let ul = document.getElementById('card')
@@ -25,5 +25,24 @@ async function loadTypeReadings(){
     
     });
 }
+
+async function submitTypeReading(){
+    const descriptionValue = document.getElementById('description_input').value
+    const prefixValue = document.getElementById('prefix_input').value
+    const minValue = document.getElementById('min_input').value
+    const maxValue = document.getElementById('max_input').value
+
+    try {
+        const status = await postTypeReading(descriptionValue, prefixValue, minValue, maxValue)
+         if(status === 200) location.reload();
+  
+    } catch (error) {
+        console.log('errorer', error)
+    }
+}
+
+const btn = document.getElementById('register__button')
+btn.addEventListener('click', submitTypeReading)
+
 
 loadTypeReadings()
