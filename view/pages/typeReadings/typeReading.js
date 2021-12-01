32aeeb4e9ba4c18e965889c636160a2426450537
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-12-01 16:59:56 -0300

    feat: add delete funciton on `typeReading`

--- view/pages/typeReadings/typeReading.js
+++ view/pages/typeReadings/typeReading.js
@@ -1,4 +1,5 @@
-import { getTypeReadings, postTypeReading } from "../../../model/server/api.js"
+import { deleteEntity, getTypeReadings, postTypeReading } from "../../../model/server/api.js"
+import { userAdmin } from "../../../viewModel/userLogin.js";
 
 async function loadTypeReadings(){
     let ul = document.getElementById('card')
@@ -16,6 +17,9 @@ async function loadTypeReadings(){
                 <div class="card__paragraph">valor máximo:<p> ${typeReading.max_value}</p></div>
                 <div class="card__paragraph">valor mínimo:<p> ${typeReading.min_value}</p></div>
             </div>
+            <button type="button" class="card__trash--button  ${userAdmin()}" value="${typeReading.description}"/>
+            <img class="card__trash" src="/view/public/icons/icon-trash.svg" alt="${typeReading.description}"></img>
+        </button>
         </div>
         `
     
@@ -41,8 +45,22 @@ async function submitTypeReading(){
     }
 }
 
+export async function deleteTypeReadingController(description) {
+    try {
+        const status = await deleteEntity('type-reading', description)
+        if(status === 200) location.reload();
+    }catch (error) {
+        console.log('ocorreu um erro')    
+    }
+}
+
+$(document).on("click", ".card__trash--button", event => {
+    deleteTypeReadingController(event.target.alt)
+})
+
 const btn = document.getElementById('register__button')
 btn.addEventListener('click', submitTypeReading)
+
 
 
 loadTypeReadings()
